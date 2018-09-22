372ca9eeabe0bcb0d13c4ab2c0c52ba8630b6a61
meru_akimbo <merukatoruayu0@gmail.com>
2018-09-22 17:49:53 +0000

    add price_idx

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,8 @@ CREATE TABLE IF NOT EXISTS sheets (
     `rank`      VARCHAR(128)     NOT NULL,
     num         INTEGER UNSIGNED NOT NULL,
     price       INTEGER UNSIGNED NOT NULL,
-    UNIQUE KEY rank_num_uniq (`rank`, num)
+    UNIQUE KEY rank_num_uniq (`rank`, num),
+    KEY price_idx (price)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS reservations (
